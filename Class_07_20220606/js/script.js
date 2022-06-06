b6cfd35a25bd90ce1594bf5593aeb31189b48be9
llprotosll <protos99@gmail.com>
2022-06-06 20:33:45 +0300

    .

---
+++ Class_07_20220606/js/script.js
@@ -0,0 +1,41 @@
+// +.-.*./
+// ||,&&,!
+// |,&,~
+
+
+456||0;
+
+console.log(456||0);
+document.getElementById("demo").innerHTML = 456||0;
+
+/*
+2 -> 0000 001(0)
+4 -> 0000 010(0)
+
+3 -> 0000 001(1)
+11 ->0000 101(1)
+
+x&0 -> 0000 0000
+x&1 -> 0000 000(x)
+(x%2) == 1
+*/
+
+// program to check if the number is even or odd
+// take input from the user
+const number = prompt("Enter a number: ");
+
+//check if the number is even
+if(number % 2 == 0) {
+    console.log("The number is even.");
+}
+
+// if the number is odd
+else {
+    console.log("The number is odd.");
+}
+
+//script to change paragraph
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+  }
--- Class_07_20220606/readme.md
+++ Class_07_20220606/readme.md
@@ -1,3 +1,6 @@
 https://javascript.info/logical-operators
 
 ![image](https://user-images.githubusercontent.com/38009737/172193406-4100e83b-27b0-48ff-8c21-178f76f120d7.png)
+
+"a".charCodeAt ();
+https://stackoverflow.com/questions/10879244/how-to-get-the-ascii-value-in-javascript-for-the-characters
